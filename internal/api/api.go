6548e947b71058ed9827f34b23540347a218233f
gorik <gorik@.com>
2024-05-30 10:44:55 +0300

    run_main_server

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"rest_api_postgres_clean/internal/inventory"
+	"strings"
 	"sync"
 )
 
@@ -27,6 +30,48 @@ type grpcServer struct {
 //:::: RUN SERVERS
 
 func (s *Server) Run(ctx context.Context) error {
+
+	ctx, cancel := context.WithCancel(ctx)
+	//:::: error channel for starting server
+	errCh := make(chan error, 2)
+
+	//::: start goroutine SERVER (http,grps)
+
+	go func() {
+		err := s.httpServer.Run(ctx, s.HTTPaddr)
+		if err != nil {
+			err = fmt.Errorf(" http Server finished with error :::%w ", err)
+		}
+		errCh <- err
+
+	}()
+	go func() {
+		err := s.grpcServer.Run(ctx, s.GRPCaddr)
+		if err != nil {
+			err = fmt.Errorf(" grpc Server finished with error :::%w ", err)
+		}
+		errCh <- err
+
+	}()
+
+	var stringErr []string
+
+	// ::: HANDLE errors
+	for i := range cap(errCh) {
+		if err := <-errCh; err != nil {
+			stringErr = append(stringErr, err.Error())
+
+			// SHUTDOWN server in case of healthy context
+			if ctx.Err() == nil {
+				s.Shutdown(context.Background())
+			}
+		}
+
+	}
+	cancel()
+
+	err := errors.New(strings.Join(stringErr, ", "))
+	return err
 
 }
 func (s *httpServer) Run(ctx context.Context, addr string) error {
